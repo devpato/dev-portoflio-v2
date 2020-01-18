35f39048ddfe0d30d45b6155b28abefc866bcb83
Pato <pevargas2730@gmail.com>
2020-01-18 05:41:07 -0500

    saturn component ts

---
+++ src/app/shared/components/saturn/saturn.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-saturn",
+  templateUrl: "./saturn.component.html",
+  styleUrls: ["./saturn.component.scss"]
+})
+export class SaturnComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

148e8eede3d8660874158e1ee299f673b5bc3907
Pato <pevargas2730@gmail.com>
2020-01-18 00:22:44 -0500

    added ngzorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,14 @@ import { SkillsComponent } from './skills/skills.component';
 import { MarsComponent } from './mars/mars.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
    declarations: [
@@ -28,9 +36,13 @@ import { FooterComponent } from './shared/components/footer/footer.component';
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      NgZorroAntdModule,
+      FormsModule,
+      HttpClientModule,
+      BrowserAnimationsModule
    ],
-   providers: [],
+   providers: [{ provide: NZ_I18N, useValue: en_US }],
    bootstrap: [
       AppComponent
    ]

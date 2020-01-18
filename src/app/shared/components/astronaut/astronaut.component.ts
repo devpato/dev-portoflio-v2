ab620981fc1150cf257fbfbb6bcfacf252b1adef
Pato <pevargas2730@gmail.com>
2020-01-18 05:19:20 -0500

    created astrnaut component

---
+++ src/app/shared/components/astronaut/astronaut.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-astronaut',
+  templateUrl: './astronaut.component.html',
+  styleUrls: ['./astronaut.component.scss']
+})
+export class AstronautComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

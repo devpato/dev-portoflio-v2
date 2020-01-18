7fb662f3b7c5990db62449bf57b7e3fd9ed300d5
Pato <pevargas2730@gmail.com>
2020-01-18 00:29:04 -0500

    migrated css to scss

--- src/app/mars/mars.component.ts
+++ src/app/mars/mars.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mars',
   templateUrl: './mars.component.html',
-  styleUrls: ['./mars.component.css']
+  styleUrls: ['./mars.component.scss']
 })
 export class MarsComponent implements OnInit {
 

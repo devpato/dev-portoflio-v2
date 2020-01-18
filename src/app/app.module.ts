193bfc7d120356d0fb1bc2046a901aca7f36fc9f
Pato <pevargas2730@gmail.com>
2020-01-17 21:29:05 -0500

    experience component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ExpirienceComponent } from './expirience/expirience.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       AboutComponent,
-      ContactComponent
+      ContactComponent,
+      ExpirienceComponent
    ],
    imports: [
       BrowserModule,

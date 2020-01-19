558239edbefbaab14f76507dc04eda02c56843c4
Pato <pevargas2730@gmail.com>
2020-01-19 02:43:07 -0500

    added new component called venus

---
+++ src/app/shared/components/venus/venus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-venus',
+  templateUrl: './venus.component.html',
+  styleUrls: ['./venus.component.scss']
+})
+export class VenusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
